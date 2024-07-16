3041a3e54af058c1cac82e6853809f7751928c78
Gede Primahadi W. Rajeg <primahadiwijaya@gmail.com>
2024-07-16 22:49:54 +0800

    Doing exercise no. 1 in Chapter 1 Section 1.8.

--- app.R
+++ app.R
@@ -3,13 +3,21 @@ library(shiny)
 ui <- fluidPage( # a layout function: it sets up the basic visual structure of the page
     
     # input control: allowing user interacts with the app by providing a value (e.g., the select box labelled "Dataset")
-		selectInput(inputId = "dataset", label = "Dataset", choices = ls("package:datasets")),
-		
+		## selectInput(inputId = "dataset", label = "Dataset", choices = ls("package:datasets")),
+	  textInput(inputId = "name", label = "What's your name?"),	
+	  
+	  numericInput(inputId = "age", label = "How old are you?", value = NA),
+    
 		# output control displaying code
-		verbatimTextOutput(outputId = "summary"),
+		## verbatimTextOutput(outputId = "summary"),
+		textOutput("greeting"),
 		
 		# output control displaying table
-		tableOutput(outputId = "table")
+		## tableOutput(outputId = "table")
+		tableOutput(outputId = "mortgage"),
+		
+		# plotOutput(outputId = "histogram") # this code needs to be added if we want the histogram output code to be shown (from exercise no. 1 in Chapter 1)
+		
 )
 
 server <- function(input, output, session) {
@@ -27,24 +35,46 @@ server <- function(input, output, session) {
   
   # prepare the "how" to Shiny to generate the output
   # the output$summary is referenced with the UI (outputId = "summary") above
-  output$summary <- renderPrint( # a render{Type} function paired with verbatimTextOutput(), a {type}Output function
+  #output$summary <- renderPrint( # a render{Type} function paired with verbatimTextOutput(), a {type}Output function
+    
+  #  {
+  #    # In this R expression ({...}), we tell shiny to store the output of summary() on the "dataset" into the output$summary
+  #    summary(dataset())
+  #  }
+    
+  #)
+  
+  output$greeting <- renderText(
     
     {
-      # In this R expression ({...}), we tell shiny to store the output of summary() on the "dataset" into the output$summary
-      summary(dataset())
+      
+      paste0("Hello ", input$name)
+      
     }
+    
     
   )
   
-  output$table <- renderTable(
+  ## output$histogram <- renderPlot(
     
-    {
+  ##  {
       
-      dataset()
+  ##    hist(rnorm(1000))
       
-    }
+  ##  }, res = 96
     
-  )
+    
+  ## )
+  
+  ## output$table <- renderTable(
+    
+  ##  {
+      
+  ##    dataset()
+      
+  ##  }
+    
+  ## )
   
 }
 
