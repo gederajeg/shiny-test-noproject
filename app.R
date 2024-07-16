6e89d3d620aa0208c722047cf1f99f58b1b07152
Gede Primahadi W. Rajeg <primahadiwijaya@gmail.com>
2024-07-16 22:01:33 +0800

    Modified the UI control.

--- app.R
+++ app.R
@@ -1,8 +1,15 @@
 library(shiny)
 
-ui <- fluidPage(
-
-		"Hello, world!"
+ui <- fluidPage( # a layout function: it sets up the basic visual structure of the page
+    
+    # input control: allowing user interacts with the app by providing a value (e.g., the select box labelled "Dataset")
+		selectInput(inputId = "dataset", label = "Dataset", choices = ls("package:datasets")),
+		
+		# output control displaying code
+		verbatimTextOutput(outputId = "summary"),
+		
+		# output control displaying table
+		tableOutput(outputId = "table")
 )
 
 server <- function(input, output, session) {
